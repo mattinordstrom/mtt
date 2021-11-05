0864da6c1a54d3d089957998d76bcf390286698f
Matti Nordström <matti.nordstrom@gmail.com>
2021-11-05 15:57:49 +0100

    Added fileinfo script

--- mtt_main.py
+++ mtt_main.py
@@ -46,6 +46,7 @@ scripts.append(scriptInfo('versions', 'DEV_TOOLS', 'Prints java, PHP, node, npm 
 scripts.append(scriptInfo('timestamp', 'DEV_TOOLS', 'Converts timestamp (ms) or date string (\"yyyy-mm-dd hh:mm:ss\"). \nIf no arg is passed the current timestamp (now) is printed.', ['?string']))
 scripts.append(scriptInfo('localkafka', 'DEV_TOOLS', 'Starts local kafka server.', []))
 
+scripts.append(scriptInfo('fileinfo', 'MISC', 'Get details about a file.', ['filepath']))
 scripts.append(scriptInfo('find', 'MISC', 'Find file with name. (Default directory is ~)', ['pattern', '?directory']))
 
 if scriptName in ['h', 'help', '-h', '-help', '--h', '--help']:
