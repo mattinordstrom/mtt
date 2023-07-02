51220417b408e27e24c613a275775cebb1997676
Matti Nordström <matti.nordstrom@gmail.com>
2023-07-02 15:23:28 +0200

    Better file name and error handling

--- mtt_scripts/savegptchat.py
+++ mtt_scripts/savegptchat.py
@@ -3,24 +3,34 @@
 import pyperclip
 import json
 import os
+from datetime import datetime
 
 BLUE = '\033[94m'
 RED = '\033[91m'
 ENDC = '\033[0m'
 
-# TODO error handling when content is not json
-
-# TODO error when content is not correct json response from GET chat.openai.com/backend-api/conversation/<ID>
-
 # Get JSON from clipboard
 json_data = pyperclip.paste()
 
 # Parse JSON
-parsed_data = json.loads(json_data)
+try:
+    parsed_data = json.loads(json_data)
+except json.JSONDecodeError:
+    print(RED + "Sorry, there was an error decoding the JSON data." + ENDC)
+    exit()
+
+if 'mapping' not in parsed_data or len(parsed_data['mapping']) <= 0:
+    print(RED + "mapping object is missing or empty in the JSON data. Did you copy the correct payload to clipboard?" + ENDC)
+    exit()
 
 # Output file
+fileNameFromChatTitle = parsed_data['title']
+fileNameFromChatTitle = ''.join(e for e in fileNameFromChatTitle if e.isalnum())
+fileNameFromChatTitle = fileNameFromChatTitle.replace("å", "a").replace("ä", "a").replace("ö", "o").replace("Å", "A").replace("Ä", "A").replace("Ö", "O")
+fileNameFromChatTitle = fileNameFromChatTitle + '_' + datetime.now().strftime("%HH%MM%SS") + '.txt'
+
 FILE_PATH = "~/Desktop/gpt_chats/"
-FILE_NAME = "gpt_chat_hist.txt" #TODO filename from title (and date)
+FILE_NAME = fileNameFromChatTitle
 output_dir_path = os.path.expanduser(FILE_PATH)
 os.makedirs(output_dir_path, exist_ok=True)
 output_file_path = os.path.join(output_dir_path, FILE_NAME)
