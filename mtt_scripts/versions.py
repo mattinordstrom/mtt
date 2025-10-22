bb85a919221c0eb5640cacb373f199dbf6f12b43
Matti Nordström <mattinordstrom@proton.me>
2025-10-22 16:30:51 +0200

    Added which

--- mtt_scripts/versions.py
+++ mtt_scripts/versions.py
@@ -6,33 +6,47 @@ RED = '\033[91m'
 GREEN = '\033[92m'
 ENDC = '\033[0m'
 
+ZWSP = "\u200B"
 
 def get_version(cmd: Sequence[str]) -> str:
     result = RED +"NOT FOUND"+ENDC
+    which = ""
     try:
         result = subprocess.check_output(cmd, text=True, stderr=subprocess.STDOUT)
         result = GREEN + result.splitlines()[0] + ENDC
+        which = subprocess.check_output(["which", cmd[0]], text=True)
     except FileNotFoundError:
         result = RED +"NOT FOUND"+ENDC
     except Exception:
         result = RED +"ERROR"+ENDC
 
-    return result
+    return result, which
 
+nodew = subprocess.check_output(["which", "node"], text=True)
+
+phpv,phpw = get_version(["php", "-r", "echo rtrim(phpversion());"])
+nodev,nodew = get_version(["node", "-v"])
+npmv,npmw = get_version(["npm", "-v"])
+javav,javaw = get_version(["java", "-version"])
+mvnv,mvnw = get_version(["mvn", "--version"])
+gov,gow = get_version(["go", "version"])
+pythonv,pythonw = get_version(["python3", "-V"])
+                           
 output = [
-    ["PHP", get_version(["php", "-r", "echo rtrim(phpversion());"])],
-    ["", ""],
-    ["NODE", get_version(["node", "-v"])],
-    ["NPM", get_version(["npm", "-v"])],
-    ["", ""],
-    ["JAVA", get_version(["java", "-version"])],
-    ["", ""],
-    ["GO", get_version(["go", "version"])],
-    ["", ""],
-    ["PYTHON", get_version(["python3", "-V"])],
-    ["", ""]
+    ["PHP", phpv, phpw],
+    [ZWSP, ZWSP, ZWSP],
+    ["NODE", nodev, nodew],
+    ["NPM", npmv, npmw],
+    [ZWSP, ZWSP, ZWSP],
+    ["JAVA", javav, javaw],
+    ["MAVEN", mvnv, mvnw],
+    [ZWSP, ZWSP, ZWSP],
+    ["GO", gov, gow],
+    [ZWSP, ZWSP, ZWSP],
+    ["PYTHON", pythonv, pythonw],
+    [ZWSP, ZWSP, ZWSP],
 ]
 
 print("\n")
-print(tabulate(output, headers=[], tablefmt="pipe"))
+print(tabulate(output, headers=[], tablefmt="simple"))
 print("\n")
